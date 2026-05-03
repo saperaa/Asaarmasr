f34be478b44bf979d7b9896d96d4ce86fa5fd40f
Mohamed Aboelela <saperaa@github.com>
2026-05-04 00:16:28 +0300

    Add Vercel Analytics for visitor and page view tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router";
 import App from "./app/App.tsx";
 import "./styles/index.css";
 import { LanguageProvider } from "./app/context/language-context.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <LanguageProvider>
       <App />
     </LanguageProvider>
+    <Analytics />
   </BrowserRouter>
 );
